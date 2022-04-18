8623bcb9fc597f10e2a438d043333848472cb341
Manuel <a20190133@pucp.edu.pe>
2022-04-18 10:38:27 -0500

    Proyecto Base

---
+++ src/main/java/com/example/ejclase5/EjClase5Application.java
@@ -0,0 +1,13 @@
+package com.example.ejclase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjClase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjClase5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejclase5/EjClase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejclase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjClase5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
