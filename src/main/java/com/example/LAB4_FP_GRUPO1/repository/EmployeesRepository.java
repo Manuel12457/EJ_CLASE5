bb80d52b6e332ba25f3e72392fd00b3db4c1d88e
Manuel <a20190133@pucp.edu.pe>
2022-04-18 11:20:18 -0500

    Modificaciones entidad empleado

--- src/main/java/com/example/LAB4_FP_GRUPO1/repository/EmployeesRepository.java
+++ src/main/java/com/example/LAB4_FP_GRUPO1/repository/EmployeesRepository.java
@@ -1,6 +1,8 @@
 package com.example.LAB4_FP_GRUPO1.repository;
 
+import com.example.LAB4_FP_GRUPO1.entity.Departments;
 import com.example.LAB4_FP_GRUPO1.entity.Employees;
+import com.example.LAB4_FP_GRUPO1.entity.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.EntityManagerBeanDefinitionRegistrarPostProcessor;
 import org.springframework.stereotype.Repository;
@@ -11,11 +13,9 @@ import java.util.List;
 public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 
     public List<Employees> findByFirstname(String firstName);
-    public List<Employees> findByLastname(String firstName);
-    public List<Employees> findBy(String firstName);
-    public List<Employees> findByFirstname(String firstName);
-    public List<Employees> findByFirstname(String firstName);
-
+    public List<Employees> findByLastname(String lastName);
+    public List<Employees> findByJob(Jobs jobs);
+    public List<Employees> findByDepartment(Departments departments);
 
 
 }
